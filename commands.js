b15129aefecd1b4b89c9c8afe99da74f158d3cb8
Bryan Bergo <bryan.l.bergo@gmail.com>
2022-11-12 15:34:22 -0800

    Update commands.js

--- commands.js
+++ commands.js
@@ -4,7 +4,7 @@ const { InteractionCollector } = require("discord.js");
 var restartCommand = 'screen -S mcServer -X stuff "stop^M" && sleep 5; screen -S mcServer -X stuff "killall -9 java^M"; screen -S mcServer -X stuff "(cd ~/Desktop/Spigot && java -jar spigot-1.19.2.jar)^M"'
 
 
-function mcRestart(interaction) 
+async function mcRestart(interaction) 
 {
 
     exec(restartCommand, async (error, stdout, stderr) => {
