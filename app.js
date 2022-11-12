b8dd662b43a45639ab86a890f19de38dfd8f146f
Bryan Bergo <bryan.l.bergo@gmail.com>
2022-11-12 15:42:18 -0800

    update

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const { Client, GatewayIntentBits } = require('discord.js');
 var secrets = require('./secrets.js');
 var commands = require('./commands.js');
 
-var restartCommand = 'screen -S mcServer -X stuff "stop^M" && sleep 5; screen -S mcServer -X stuff "killall -9 java^M"; screen -S mcServer -X stuff "(cd ~/Desktop/Spigot && java -jar spigot-1.19.2.jar)^M"'
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
@@ -15,20 +13,8 @@ client.on('interactionCreate', async interaction => {
 
   if (interaction.commandName === 'mc-restart') {
     //code for restarting minecraft server
-    exec(restartCommand, async(error, stdout, stderr) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-      if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-      }
-      console.log(`stdout: ${stdout}`);
-
-      await interaction.reply('Successfully restarted minecraft server');
-  });
-
+    commands.mcRestart();
+    await interaction.reply('Success');
   }
 });
 
--- commands.js
+++ commands.js
@@ -4,10 +4,22 @@ const { InteractionCollector } = require("discord.js");
 var restartCommand = 'screen -S mcServer -X stuff "stop^M" && sleep 5; screen -S mcServer -X stuff "killall -9 java^M"; screen -S mcServer -X stuff "(cd ~/Desktop/Spigot && java -jar spigot-1.19.2.jar)^M"'
 
 
-async function mcRestart(interaction) 
+function mcRestart(interaction) 
 {
 
-    
+    exec(restartCommand, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+
+        
+    });
 }
 
 module.exports = {mcRestart}
