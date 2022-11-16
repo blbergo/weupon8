c4d3e90f05dfb15edb180849d53e83403cf090c8
Bryan Bergo <bryan.l.bergo@gmail.com>
2022-11-15 23:09:22 -0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
   const channel = client.channels.cache.find(channel => channel.name == "bot-commands");
-  var urls = "Linux sucessfully restarted \n New Urls: \n" + await commands.getUrls();
+  var urls = "Linux sucessfully restarted \n New Urls: \n" 
+  urls += await commands.getUrls();
   channel.send(urls);
 });
 
